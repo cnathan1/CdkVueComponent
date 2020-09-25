6f29d5ea3df958b8b59692607823394a82d1420e
Nathani <chankin@a483e7980903.ant.amazon.com>
2020-09-25 18:26:54 -0400

    reverted back main.ts

--- vue-web-component-app/src/main.ts
+++ vue-web-component-app/src/main.ts
@@ -1,8 +1,12 @@
-import Vue from 'vue';
-import wrap from '@vue/web-component-wrapper';
-import App from './App.vue';
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-const WrappedElement = wrap(Vue, App);
-window.customElements.define('hierarchy-builder', WrappedElement);
+Vue.config.productionTip = false
 
-Vue.config.productionTip = false;
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
